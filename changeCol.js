70593ccd84cbb29405a5f10637292c910537652e
Anna Kozlova <kozlova9v@mail.ru>
2017-07-16 20:35:58 +0200

    ManyCaaaats

---
+++ changeCol.js
@@ -0,0 +1,6 @@
+
+function changeCol(color){
+            var cat = document.getElementById("cat");
+            if (color=='blue') cat.style.color='#6C81E6';
+            else cat.style.color="white";
+        }
