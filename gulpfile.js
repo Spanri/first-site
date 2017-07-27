f0aa333da9058f1e4c84774e4d01d6a31ee471a5
Anna Kozlova <kozlova9v@mail.ru>
2017-07-28 00:39:08 +0200

    gulp, gulp-haml

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var haml = require('gulp-haml');
+
+gulp.task('mytask', function() {
+    console.log('Привет, я таск!');
+});
+
+gulp.task('haml', function () {
+  return gulp.src('haml/*.haml')
+    .pipe(haml())
+    .pipe(gulp.dest('www2'));
+})
